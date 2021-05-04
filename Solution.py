9c4015de7b191591a7fedc74a87751645fd15fa9
monty502 <m08502@hotmail.com>
2021-05-03 22:41:39 -0400

    Explanation added

--- Solution.py
+++ Solution.py
@@ -2,10 +2,18 @@ def num_of_paths_to_dest(n):
   # there's only 1 path to reach every cell in row 0
   dp = [1] * n
   
+  # from row 1 on, dp[col] represents number of paths to square[row, col]
   for row in range(1, n):
     for col in range(row, n):
-      # if cell is on diagnal line, the only paths are from below
-      # otherwise, the paths are either from below, or from left
+      # if the square is on diagnal line, the only paths are from below
+      # otherwise, the paths are either from below, or from left:
+      #             5
+      #             ^
+      #         2 > 5
+      #         ^   ^
+      #     1 > 2 > 3
+      #     ^   ^   ^
+      # 1 > 1 > 1 > 1
       if col > row:
         dp[col] += dp[col - 1]
   
